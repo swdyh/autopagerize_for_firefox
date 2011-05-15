acf0e318f15a6607eb1efbd4e69d86e9055d4c0e
X Kerman <xKhorasan@gmail.com>
2011-05-15 22:14:48 +0900

    Add-on SDK 1.0b5 で dupricate warning が出るのを修正

--- data/context_menu.js
+++ data/context_menu.js
@@ -1,13 +1,13 @@
-on('context', function (node) {
+self.on('context', function (node) {
     return document.getElementById('autopagerize_message_bar');
 })
-on('click', function (node, data) {
+self.on('click', function (node, data) {
     if (data == 'toggle') {
         var ev = document.createEvent('Event');
         ev.initEvent('AutoPagerizeToggleRequest', true, false)
         document.dispatchEvent(ev)
     }
     else if (data == 'config') {
-        postMessage('show_config_panel')
+        self.postMessage('show_config_panel')
     }
 })
