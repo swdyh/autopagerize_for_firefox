e427c07b08f1f52f12ce298218e7b42cdbd442de
swdyh <youhei@gmail.com>
2011-06-17 11:32:24 +0900

    on/off dispatch all tabs

--- data/context_menu.js
+++ data/context_menu.js
@@ -3,9 +3,10 @@ self.on('context', function (node) {
 })
 self.on('click', function (node, data) {
     if (data == 'toggle') {
-        var ev = document.createEvent('Event');
-        ev.initEvent('AutoPagerizeToggleRequest', true, false)
-        document.dispatchEvent(ev)
+        // var ev = document.createEvent('Event');
+        // ev.initEvent('AutoPagerizeToggleRequest', true, false)
+        // document.dispatchEvent(ev)
+        self.postMessage('toggle')
     }
     else if (data == 'config') {
         self.postMessage('show_config_panel')
